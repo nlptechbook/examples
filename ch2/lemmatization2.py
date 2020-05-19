3f6c796089e92f89fcf727d15f2de70e1a8db854
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2020-05-19 11:42:33 -0400

    Delete lemmatization2.py

--- ch2/lemmatization2.py
+++
@@ -1,8 +0,0 @@
-import spacy
-from spacy.symbols import ORTH, LEMMA
-nlp = spacy.load('en')
-doc = nlp(u'I am flying to Frisco')
-print([w.text for w in doc])
-special_case = [{ORTH: u'Frisco', LEMMA: u'San Francisco'}]
-nlp.tokenizer.add_special_case(u'Frisco', special_case)
-print([w.lemma_ for w in nlp(u'I am flying to Frisco')])
