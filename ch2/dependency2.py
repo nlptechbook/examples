be052c31aa53ae1132a4187c062599f51b32b68b
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2020-05-19 11:53:58 -0400

    Delete dependency2.py

--- ch2/dependency2.py
+++
@@ -1,5 +0,0 @@
-import spacy
-nlp = spacy.load('en')
-doc = nlp(u'I have flown to LA. Now I am flying to Frisco.')
-for sent in doc.sents:
-  print([w.text for w in sent if w.dep_ == 'ROOT' or w.dep_ == 'pobj'])
