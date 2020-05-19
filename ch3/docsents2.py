b77d264093967b7c780c1f8a88ebca91c13e029a
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2020-05-19 11:01:01 -0400

    Delete docsents2.py

--- ch3/docsents2.py
+++
@@ -1,6 +0,0 @@
-import spacy
-nlp = spacy.load('en')
-doc = nlp(u'A severe storm hit the beach. It started to rain.')
-for i,sent in enumerate(doc.sents):
-  if i==1 and sent[0].pos_== 'PRON':
-    print('The second sentence begins with a pronoun.')
