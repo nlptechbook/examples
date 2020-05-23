3d16588634731c560033008f117128ea1b21a439
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2020-05-23 07:26:11 -0400

    Using leftward syntactic children of the noun to extract the noun chunk

---
+++ ch3/trythis.py
@@ -0,0 +1,10 @@
+import spacy
+nlp = spacy.load('en')
+doc = nlp(u'A noun chunk is a phrase that has a noun as its head.')
+for token in doc:
+ if token.pos_=='NOUN':
+   chunk = ''
+   for w in token.lefts:
+     chunk = chunk + w.text + ' '
+   chunk = chunk + token.text
+   print(chunk)
